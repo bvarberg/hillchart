12692938c499b50816277cf4fa932b732d9fa09f
Byron Varberg <byronvarberg@gmail.com>
2022-08-20 07:50:45 -0500

    Scaffold with vite

---
+++ src/Demo.tsx
@@ -0,0 +1,7 @@
+export function Demo() {
+  return (
+    <div>
+      <h1>Vite + React</h1>
+    </div>
+  );
+}
---
+++ vite.config.ts
@@ -0,0 +1,23 @@
+import { defineConfig } from "vite";
+import { resolve } from "path";
+import react from "@vitejs/plugin-react";
+
+// https://vitejs.dev/config/
+export default defineConfig({
+  plugins: [react()],
+  build: {
+    lib: {
+      entry: resolve(__dirname, "lib/module.ts"),
+      name: "hillchart",
+      fileName: "hillchart",
+    },
+    rollupOptions: {
+      external: ["react", "react-dom"],
+      output: {
+        globals: {
+          react: "React",
+        },
+      },
+    },
+  },
+});
