10ae179e36a52ff6c413fe0b3285d8db80768f27
Byron Varberg <byronvarberg@gmail.com>
2022-08-26 19:16:36 -0500

    Use scales from lib

--- src/demo/Demo.tsx
+++ src/demo/Demo.tsx
@@ -1,28 +1,13 @@
-import { Milestone, Hill, curve } from "../lib";
-import { scaleLinear } from "d3-scale";
-import { compose, range, map } from "ramda";
-
-function useScales(height: number, width: number) {
-  const toX = scaleLinear().domain([0, 1]).range([0, width]);
-  const toCurve = scaleLinear().domain([0, 1]).range([-1, 1]);
-  const toY = scaleLinear().domain([0, 1]).range([height, 0]);
-
-  const x = toX;
-  const y = compose(toY, curve, toCurve);
-
-  return {
-    x,
-    y,
-  };
-}
+import { useScales, Milestone, Hill, curve } from "../lib";
+import { range, map } from "ramda";
 
 export function Demo() {
   const height = 300;
   const width = 600;
 
-  const { x, y } = useScales(height, width);
+  const { x, y } = useScales({ height, width });
 
-  const numberOfPoints = 10;
+  const numberOfPoints = 3;
   const input = map((n) => n / numberOfPoints, range(0, numberOfPoints + 1));
   const output = map(
     (n) => ({
@@ -34,6 +19,7 @@ export function Demo() {
 
   return (
     <div>
+      <code>{JSON.stringify(output, null, 2)}</code>
       <svg
         width={width}
         height={height}
@@ -44,7 +30,7 @@ export function Demo() {
         <Hill x={x} y={y} />
         {map(
           (point) => (
-            <Milestone centerPoint={point} radius={10} />
+            <Milestone key={point.x} centerPoint={point} radius={10} />
           ),
           output
         )}
