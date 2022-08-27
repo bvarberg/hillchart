8055dcd1dea7bcede89817dfe818868b7d10aa22
Byron Varberg <byronvarberg@gmail.com>
2022-08-26 21:47:09 -0500

    Progress between 0 and 1

--- src/lib/schemas/datapoint.ts
+++ src/lib/schemas/datapoint.ts
@@ -2,7 +2,7 @@ import { z } from "zod";
 
 export const Datapoint = z.object({
   id: z.string(),
-  progress: z.number().int().min(0).max(100),
+  progress: z.number().min(0).max(1),
   label: z.string(),
 });
 
